d640ebf9c0ee65f89b087cf92a642035406c68b1
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-27 20:12:17 +0200

    commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => {});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen();
+app.listen(PORT, () => console.log(`Server just started on port ${PORT}`));
