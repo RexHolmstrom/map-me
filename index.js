43bbb808a73bce80d159caaf3768002aead09a0d
rexholmstrom <rexholmstrom96@gmail.com>
2020-10-01 22:20:14 +0200

    update fix

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ app.get("/api/members", (req, res) => res.json(members));
 //Single
 
 app.get("/api/members/:id", (req, res) => {
-  res.send(req.params.id);
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No member found with the id of ${req.params.id}` });
+  }
 });
 
 // Static
