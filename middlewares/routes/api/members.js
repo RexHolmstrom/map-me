cf923aa82c336034752f8fc5c5d425771444c2cc
rexholmstrom <rexholmstrom96@gmail.com>
2020-10-02 19:50:54 +0200

    folder fix

--- middlewares/routes/api/members.js
+++ middlewares/routes/api/members.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-app.get("/api/members", (req, res) => res.json(members));
+router.get("/api/members", (req, res) => res.json(members));
 
 //Single
 
-app.get("/api/members/:id", (req, res) => {
+router.get("/api/members/:id", (req, res) => {
   const found = members.some((member) => member.id === parseInt(req.params.id));
 
   if (found) {
