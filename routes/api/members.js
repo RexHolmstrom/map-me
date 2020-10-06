8d866c68046f0d89ea7d0f813000a9f81859f158
rexholmstrom <rexholmstrom96@gmail.com>
2020-10-06 10:53:16 +0200

    fix delete

--- routes/api/members.js
+++ routes/api/members.js
@@ -57,4 +57,23 @@ router.put("/:id", (req, res) => {
   }
 });
 
+//Delete
+
+router.delete("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      msg: "Member Deleted",
+      members: members.filter(
+        (member) => member.id !== parseInt(req.params.id)
+      ),
+    });
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No member found with the id of ${req.params.id}` });
+  }
+});
+
 module.exports = router;
